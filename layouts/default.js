b3058b78dddb67c7b2213bdff1ae62060f3c88c7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-10 15:22:05 +0800

    testing: testing (#7)

--- layouts/default.js
+++ layouts/default.js
@@ -2,7 +2,7 @@ import Navbar from '../components/navbar'
 
 export default function Layout ({ children }) {
   return (
-    <div className="flex justify-center w-full min-w-full">
+    <div className="flex justify-center w-screen min-w-full">
       <Navbar />
       <div className="flex w-full h-auto">
         { children }
