1e055ba4c81eaee0e43e58eef7d484070fe0bef4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-10 14:29:18 +0800

    feat: add themeprovider and nprogress plugins

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import '../styles/tailwind.css'
+import { ThemeProvider } from 'next-themes'
+import NextNProgress from '../lib/nprogress'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider attribute="class">
+        <NextNProgress />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
