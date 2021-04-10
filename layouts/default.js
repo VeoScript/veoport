a2908ee9704c49bea27b9a6908f7e85cffd65f9a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-10 14:39:30 +0800

    style: change layout (#4)

--- layouts/default.js
+++ layouts/default.js
@@ -2,9 +2,9 @@ import Navbar from '../components/navbar'
 
 export default function Layout ({ children }) {
   return (
-    <div className="flex flex-col justify-center w-full bg-white dark:bg-[#232733]">
+    <div className="flex flex-col justify-center">
       <Navbar />
-      <div className="flex h-screen items-center">
+      <div className="flex justify-center w-full h-screen items-center bg-white dark:bg-[#232733]">
         { children }
       </div>
     </div>
