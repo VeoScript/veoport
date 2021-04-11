0148d6794c687c582d54b149b2802ab6ce04662d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-11 16:37:09 +0800

    style: update layout

--- layouts/default.js
+++ layouts/default.js
@@ -2,11 +2,13 @@ import Navbar from '../components/navbar'
 
 export default function Layout ({ children }) {
   return (
-    <div className="flex justify-center w-screen min-w-full">
-      <Navbar />
-      <div className="flex w-full h-auto">
-        { children }
+    <>
+      <div className="flex justify-center min-w-full">
+        <Navbar />
+        <div className="flex w-full h-auto">
+          { children }
+        </div>
       </div>
-    </div>
+    </>
   )
 }
