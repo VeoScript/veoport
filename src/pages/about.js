0c1569245990dd93950d1c25118552d5994ed420
Joshua Galit <joshuaimalay@gmail.com>
2021-04-12 20:40:51 +0800

    fix: separate json data

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,44 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '~/layouts/default'
+import { experience, skills } from '~/static/about'
 
 export default function About() {
-  const experience = [
-    {
-      company: 'Specialty Pulp Manufacturing, Inc.',
-      website: 'https://www.spmi-pulp.com/',
-      position: 'IT Specialist',
-      hired: 'February 2020 - Present'
-    },
-    {
-      company: 'Puregold Price Club, Inc.',
-      website: 'https://www.puregold.com.ph/',
-      position: 'Graphic Artist',
-      hired: 'August 2019 - December 2019'
-    }
-  ]
-  const skills = [
-    {
-      category: 'Web Development',
-      percentage: '95%',
-      width: 'w-11/12'
-    },
-    {
-      category: 'Software Development',
-      percentage: '90%',
-      width: 'w-10/12'
-    },
-    {
-      category: 'Mobile App Development',
-      percentage: '80%',
-      width: 'w-9/12'
-    },
-    {
-      category: 'Computer Networking',
-      percentage: '100%',
-      width: 'w-full'
-    }
-  ]
+  
   return (
     <>
       <Head>
