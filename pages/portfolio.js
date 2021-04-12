bacb6a1915d0165610693bacf2fa47e14ecb1603
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-12 17:13:36 +0800

    testing: add responsive designs

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,8 +5,8 @@ export default function Portfolio() {
     <>
       <Layout>
         <div className="flex flex-col md:m-0 -mt-5 justify-center mx-auto w-full h-screen items-center bg-white dark:bg-[#232733]">
-          <div className="flex flex-row space-x-3">
-            <p className="text-lg">THIS IS PORFOLIO PAGE</p>
+          <div className="flex flex-row items-center space-x-3">
+            <p className="md:text-lg text-xs">THIS IS PORTFOLIO PAGE</p>
             <span className="flex h-auto items-center font-bold text-xs text-[#333] rounded-full bg-yellow-300 px-3 py-1">Coming Soon.</span>
           </div>
         </div>
