b833186347ae0f3b0003d08843d760f08bc9e1f1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-12 09:19:34 +0800

    style: add flex-col

--- layouts/default.js
+++ layouts/default.js
@@ -3,7 +3,7 @@ import Navbar from '../components/navbar'
 export default function Layout ({ children }) {
   return (
     <>
-      <div className="flex justify-center min-w-full">
+      <div className="flex flex-col justify-center min-w-full mx-auto w-full h-screen items-center bg-white dark:bg-[#232733]">
         <Navbar />
         <div className="flex w-full h-auto">
           { children }
