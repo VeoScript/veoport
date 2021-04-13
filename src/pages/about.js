8c26e099a804dded1463ede45f0ef9bc5752e9a7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-13 10:28:07 +0800

    Feat/add aboutpage technologies (#32)
    * feat: add photo of mine
    
    * feat: add technologies
    
    * chore: add technologies json data

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '~/layouts/default'
-import { experience, skills } from '~/static/about'
+import { experience, skills, technologies } from '~/static/about'
 
 export default function About() {
   
@@ -14,13 +14,24 @@ export default function About() {
         <div className="hidden md:flex flex-col pb-10 mx-5 justify-center items-center w-full">
           <div className="flex flex-row justify-between w-full max-w-5xl space-x-10 space-y-2">
             <div className="flex justify-start mx-auto md:mt-0 mt-8 w-1/2">
-              <img className="w-full h-72 rounded-sm bg-gray-900 object-cover text-white" src="https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/IMG20210412172311.jpg?alt=media&token=1070baad-d9b0-4dab-98e4-75f1153d2ef8" alt="Picture of Jerome Villaruel" />
+              <img className="w-full h-72 rounded-sm bg-gray-900 object-cover text-white" src="/me.jpg" alt="Picture of Jerome Villaruel" />
             </div>
-            <div className="mx-auto md:w-1/2 w-full space-y-2">
-              <h1 className="font-bold text-xl">About Me</h1>
-              <p className="text-justify text-base">
-                Hello! My name is Jerome Villaruel, I'm twenty-two years old from Philippines. I create stunning & functional websites for small businesses remotely and I know how to setup LAN connection or other network services for your business.
-              </p>
+            <div className="mx-auto md:w-1/2 w-full space-y-5">
+              <div className="space-y-1">
+                <h1 className="font-bold text-lg">About Me</h1>
+                <p className="text-justify text-sm">
+                  Hello! My name is Jerome Villaruel, I'm a developer based Philippines. I create stunning & functional websites for small businesses remotely and I know how to set up LAN connection or other network services for your business.
+                  I'm hardworking  and work on my own initiative and available for interesting freelance work projects.
+                </p>
+              </div>
+              <div className="space-y-2">
+                <h1 className="font-bold text-base">Technologies</h1>
+                <div className="flex flex-row text-sm space-x-3">
+                  {technologies.map(({ name, icon }, i) => (
+                    <div className={`${ name }`} key={i}>{ icon }</div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
           <hr className="w-full max-w-5xl my-5 border border-gray-100 dark:border-gray-700" />
@@ -40,7 +51,7 @@ export default function About() {
                     <div className="flex flex-row items-center">
                       <Link href={ website }>
                         <a target="_blank">
-                          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                          <svg className="w-5 h-5 text-[#333] dark:text-gray-200 transition ease-in-out duration-300 transform hover:rotate-45" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                         </a>
                       </Link>
                     </div>
@@ -69,13 +80,20 @@ export default function About() {
         <div className="md:hidden flex flex-col pb-10 mx-3 justify-center items-start h-screen w-full">
           <div className="flex flex-col items-center my-5 w-full h-full overflow-y-auto">
             <div className="mx-3">
-              <img className="w-full h-72 rounded-sm bg-gray-900 object-cover text-white" src="https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/IMG20210412172311.jpg?alt=media&token=1070baad-d9b0-4dab-98e4-75f1153d2ef8" alt="Picture of Jerome Villaruel" />
+              <img className="w-full h-72 rounded-sm bg-gray-900 object-cover text-white" src="/me.jpg" alt="Picture of Jerome Villaruel" />
             </div>
             <div className="mx-3 my-3">
-              <h1 className="font-normal text-sm">About Me</h1>
+              <h1 className="font-normal text-sm my-2">About Me</h1>
               <p className="font-light text-justify text-xs">
-                Hello! My name is Jerome Villaruel, I'm twenty-two years old from Philippines. I create stunning & functional websites for small businesses remotely and I know how to setup LAN connection or other network services for your business.
+                Hello! My name is Jerome Villaruel, I'm a developer based Philippines. I create stunning & functional websites for small businesses remotely and I know how to set up LAN connection or other network services for your business.
+                I'm hardworking  and work on my own initiative and available for interesting freelance work projects.
               </p>
+              <h1 className="font-normal text-sm my-3">Technologies</h1>
+              <div className="flex flex-row space-x-3">
+                {technologies.map(({ name, icon }, i) => (
+                  <div className={`${ name }`} key={i}>{ icon }</div>
+                ))}
+              </div>
             </div>
             <hr className="w-11/12 my-1 border border-gray-100 dark:border-gray-700" />
             <div className="mx-auto my-3 w-full space-y-3">
@@ -104,7 +122,7 @@ export default function About() {
                   <div className="flex flex-row items-center">
                     <Link href={ website }>
                       <a target="_blank">
-                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                        <svg className="w-5 h-5 text-[#333] dark:text-gray-200 transition ease-in-out duration-300 transform hover:rotate-45" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                       </a>
                     </Link>
                   </div>
