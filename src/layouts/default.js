9622b1583b072440ec3b1e02e7b83426d44a91d5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-14 11:17:03 +0800

    style: add overflow-y-auto

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,7 +2,7 @@ import Navbar from '~/components/navbar'
 
 export default function Layout ({ children }) {
   return (
-    <div className="flex flex-col justify-center min-w-full mx-auto w-full h-screen items-center transition ease-in-out duration-700 bg-white dark:bg-[#232733]">
+    <div className="flex flex-col justify-center items-center w-full h-screen transition ease-in-out duration-700 overflow-y-auto bg-white dark:bg-[#232733]">
       <Navbar />
       <div className="flex w-full h-auto">
         { children }
