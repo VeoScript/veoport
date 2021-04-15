4b875e46dce827240ead9f562bf4b2b486b9c6a4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-15 12:41:17 +0800

    feat: portfolio page design (#45)
    * style: remove bg-color in work-experience card
    
    * feat: add portfolio layout
    
    * feat: create portfolio json data

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,15 +38,15 @@ export default function About() {
           <hr className="w-full max-w-5xl my-3 border border-gray-100 dark:border-gray-700" />
           <div className="flex flex-row justify-between w-full max-w-5xl space-x-10">
             <div className="flex flex-col justify-start w-full max-w-lg space-y-3">
-              <h1 className="font-bold text-lg ml-3">Skills</h1>
+              <h1 className="font-bold text-lg">Skills</h1>
               <div className="flex flex-col space-y-3">
                 {skills.map(({ category, percentage, width }, i) => (
-                  <div className="percentage mx-3 space-y-1" key={i}>
+                  <div className="percentage space-y-1" key={i}>
                     <div className="flex flex-row justify-between">
                       <span className="font-light text-xs">{ category }</span>
                       <span className="font-light text-xs">{ percentage }</span>
                     </div>
-                    <div className="relative w-full h-5 rounded-full bg-gray-300 dark:bg-gray-500">
+                    <div className="relative w-full h-5 rounded-full bg-gray-300 dark:bg-gray-600">
                       <span className={`absolute h-5 rounded-full bg-[#62A9FF] ${ width }`}></span>
                     </div>
                   </div>
@@ -57,7 +57,7 @@ export default function About() {
               <h1 className="font-bold text-lg ml-3">Work Experience</h1>
               <div className="flex flex-col space-y-1.5">
                 {experience.map(({ company, icon, website, position, hired }, i) => (
-                  <div className="flex flex-row items-center justify-between bg-gray-200 dark:bg-gray-900 px-5 py-3 rounded-full" key={i}>
+                  <div className="flex flex-row items-center justify-between border-2 border-gray-300 dark:border-gray-700 px-5 py-3 rounded-full" key={i}>
                     <div className="flex flex-row items-center">
                       <div>
                         <img className="w-10 h-10" src={icon} />
@@ -68,14 +68,14 @@ export default function About() {
                         </div>
                         <div className="font-light text-sm ml-3">
                           { position }
-                          <span className="ml-2 px-2 bg-gray-300 dark:bg-gray-800 text-[#333] dark:text-gray-300 rounded-full font-semibold text-xs">{ hired }</span>
+                          <span className="px-2 rounded-full font-semibold text-xs">- { hired }</span>
                         </div>
                       </div>
                     </div>
                     <div className="flex flex-row items-center">
                       <Link href={ website }>
                         <a target="_blank">
-                          <svg className="w-5 h-5 text-[#333] dark:text-gray-300 transition ease-in-out duration-300 transform hover:rotate-45" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                          <svg className="w-5 h-5 text-[#333] dark:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                         </a>
                       </Link>
                     </div>
@@ -140,7 +140,7 @@ export default function About() {
                   <div className="flex flex-row items-center">
                     <Link href={ website }>
                       <a target="_blank">
-                        <svg className="w-5 h-5 text-[#333] dark:text-gray-300 transition ease-in-out duration-300 transform hover:rotate-45" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                        <svg className="w-5 h-5 text-[#333] dark:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                       </a>
                     </Link>
                   </div>
---
+++ src/static/portfolio.js
@@ -0,0 +1,79 @@
+export const projects = [
+  {
+    name: 'veoport',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/veoport.png?alt=media&token=e2101ac8-13df-404d-a406-905bc79af1f6',
+    description: 'Veo Portal, my official portfolio website. Using Next JS and Strapi HCMS',
+    link: 'https://github.com/VeoScript/spmi-health-declaration',
+    demo: 'https://www.jeromevillaruel.ml'
+  },
+  {
+    name: 'spmi-health-declaration-android',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/spmi-android-full.jpg?alt=media&token=4dfebb5b-5230-49d1-a396-2cf300e0a635',
+    description: 'SPMI Covid-19 Health Declaration System in Android & iOS mobile application version, using Xamarin C#.',
+    link: 'https://github.com/VeoScript/spmi-health-declaration-android',
+    demo: '/'
+  },
+  {
+    name: 'spmi-health-declaration',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/chd.png?alt=media&token=a366ac29-a0d0-4f87-b47d-05559e124db5',
+    description: 'This webapp is for covid19 health declaration system for the employees of Specialty Pulp Manufacturing Inc. using Vue JS Single Page Application, Backend by Hasura GraphQL',
+    link: 'https://github.com/VeoScript/spmi-health-declaration',
+    demo: 'https://spmi-health-declaration.vercel.app/'
+  },
+  {
+    name: 'ESL-Learning-Center',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/airclass.png?alt=media&token=025e2ae8-7a02-4eb4-b8c4-1ab25e272a32',
+    description: 'Victor Air Class Solution, Inc. Learning English Language Through Video Calls. In Asia. Using Vue JS Single Page Application Backend by Hasura GraphQL',
+    link: 'https://github.com/VeoScript/ESL-Learning-Center',
+    demo: 'https://airclass.ml/'
+  },
+  {
+    name: 'jeromevillaruel-official',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/officialwebsite.png?alt=media&token=3460df22-e103-49b6-b39c-fe55b9e40ace',
+    description: 'My official website with Freedom Wall features, using Vue JS and backend by Hasura GraphQL.',
+    link: 'https://github.com/VeoScript/jeromevillaruel-official',
+    demo: 'https://jeromevillaruel.vercel.app/'
+  },
+  {
+    name: 'fixrhythm',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/Fixrhythm.png?alt=media&token=a98c8d6d-07f4-4697-9e6f-1b6129e3eff8',
+    description: 'Introduction to Fixrhythm. Social media for musicians and poets.',
+    link: 'https://github.com/VeoScript/fixrhythm',
+    demo: 'https://fixrhythm.vercel.app/'
+  },
+  {
+    name: 'contax-web',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/contax.png?alt=media&token=e2a636fc-ff1f-4a77-9036-6aad5dfcd842',
+    description: 'Introduction to Contax on Web. Run on your local machine phonebook like webapp with notes and photo gallery features.',
+    link: 'https://github.com/VeoScript/contax-web',
+    demo: '/'
+  },
+  {
+    name: 'GoRehab',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/gorehab.png?alt=media&token=40091b26-c055-4bf2-8c62-86f0fd4e3998',
+    description: 'GoRehab official website of a private sector rehabilitation center, using PHP and MySQL.',
+    link: 'https://github.com/VeoScript/GoRehab',
+    demo: 'https://gorehab.000webhostapp.com/webpage.php'
+  },
+  {
+    name: 'DAFENHS-Automated-Enrollment-System',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/automatedenrollment.png?alt=media&token=5a649369-467e-4423-a9e7-35205505cbac',
+    description: 'This program was implemented in DAFENHS Don Agustin F. Escaño National High School, using Visual Basic and SQL Server.',
+    link: 'https://github.com/VeoScript/DAFENHS-Automated-Enrollment-System',
+    demo: '/'
+  },
+  {
+    name: 'Automated-Parking-Monitoring-System',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/parkingmonitoring.png?alt=media&token=91f5e6bc-9c1c-44bd-992a-6babbac49469',
+    description: 'Parking Lot Monitoring System this program is available to download, you can use this to your parking lot area. This system is using Visual Basic and SQL Server.',
+    link: 'https://github.com/VeoScript/Automated-Parking-Monitoring-System',
+    demo: '/'
+  },
+  {
+    name: 'KPHN-Kapihan',
+    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/kphn.png?alt=media&token=e39de51a-0244-4674-84d7-3c3f07fcdc8c',
+    description: 'Coffee shop inventory & POS. Using Visual Basic and SQL Server.',
+    link: 'https://github.com/VeoScript/KPHN-Kapihan',
+    demo: '/'
+  }
+]
