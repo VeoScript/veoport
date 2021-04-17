ae3e8ec17f47efdaec4fcc2b52d5462c07f3385e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-17 10:16:56 +0800

    feat: add messenger chat-bot

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,10 +1,17 @@
 import Navbar from '~/components/navbar'
+import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 export default function Layout ({ children }) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen transition ease-in-out duration-700 overflow-y-auto bg-white dark:bg-[#232733]">
       <Navbar />
       <div className="flex w-full h-auto">
+        <div className="flex">
+          <MessengerCustomerChat
+            pageId="101914628707986"
+            appId="3871367752945149"
+          />
+        </div>
         { children }
       </div>
     </div>
