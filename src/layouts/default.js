1a98132bccbd10f59a70e39abc6b2e3df5769882
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-17 11:50:16 +0800

    style: add hidden and md:block

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -6,14 +6,11 @@ export default function Layout ({ children }) {
     <div className="flex flex-col justify-center items-center w-full h-screen transition ease-in-out duration-700 overflow-y-auto bg-white dark:bg-[#232733]">
       <Navbar />
       <div className="flex w-full h-auto">
-        <div className="relative">
-          <div className="absolute">
-            <MessengerCustomerChat
-              pageId="101914628707986"
-              appId="3871367752945149"
-              minimized="true"
-            />
-          </div>
+        <div className="hidden md:block">
+          <MessengerCustomerChat
+            pageId="101914628707986"
+            appId="3871367752945149"
+          />
         </div>
         { children }
       </div>
