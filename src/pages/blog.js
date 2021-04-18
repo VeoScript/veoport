dcf1bd186297face155b71c5c1d75407df07960c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-18 12:58:20 +0800

    style: card component design

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,28 @@
+import Head from 'next/head'
 import Layout from '~/layouts/default'
 import { fetchAPI } from '~/lib/strapi/api'
 import Posts from '~/components/posts'
 
 export default function Others({ articles, homepage }) {
   return (
-    <Layout>
-      <div className="flex flex-col items-center px-5 md:p-0 h-screen w-full">
-        <div className="pb-20">
-          <div className="flex flex-col py-5">
-            <h1 className="font-bold text-lg">Jerome Villaruel</h1>
-            <p className="font-light text-sm">{ homepage.hero.title }</p>
-          </div>
-          <div className="flex space-y-5">
-            <Posts articles={articles} />
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Layout>
+        <div className="flex flex-col items-center px-5 md:p-0 h-screen w-full">
+          <div className="pb-20">
+            <div className="flex flex-col py-5">
+              <h1 className="font-bold text-lg">Jerome Villaruel</h1>
+              <p className="font-light text-sm">{ homepage.hero.title }</p>
+            </div>
+            <div className="flex space-y-5">
+              <Posts articles={articles} />
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
