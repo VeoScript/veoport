a2befed9e4bce3ea4dac307cbbf82d485c62fd36
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-18 12:47:12 +0800

    refactor: change main layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,29 @@
 import '~/styles/globals.css'
 import '~/styles/tailwind.css'
+import App from 'next/app'
+import { createContext } from 'react'
+import { fetchAPI } from '~/lib/strapi/api'
 import { ThemeProvider } from 'next-themes'
 import NextNProgress from '~/lib/nprogress'
 
+export const GlobalContext = createContext({})
+
 function MyApp({ Component, pageProps }) {
+  const { global } = pageProps
   return (
     <ThemeProvider attribute="class">
       <NextNProgress />
-      <Component {...pageProps} />
+      <GlobalContext.Provider value={ global }>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
     </ThemeProvider>
   )
+}
+
+MyApp.getInitialProps = async(ctx) => {
+  const appProps = await App.getInitialProps(ctx)
+  const global = await fetchAPI('/global')
+  return { ...appProps, pageProps:{ global } }
 }
 
 export default MyApp
