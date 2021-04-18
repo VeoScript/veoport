19d50d697e9663b53aef847aa525d38b1cbc9d5c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-18 12:58:59 +0800

    style: card component design (#81)

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ import { getStrapiMedia } from '~/lib/strapi/media'
 
 export default function Card({ article }) {
   return (
-    <div className="flex flex-col justify-center w-full bg-gray-100 dark:bg-gray-900">
+    <div className="flex flex-col justify-center w-full rounded-lg bg-gray-100 dark:bg-gray-900">
       <div className="flex flex-row items-center space-x-3 px-3 py-3">
         <Profiles author={article.author.picture} />
         <div className="flex flex-col">
@@ -27,7 +27,7 @@ export default function Card({ article }) {
       <div className="flex flex-row items-center justify-between p-3">
         <div className="flex flex-col">
           <p className="font-bold text-xl capitalize">{article.title}</p>
-          <p className="font-semibold text-sm uppercase">{article.category.name}</p>
+          <p className="font-semibold text-sm text-gray-500 uppercase">{article.category.name}</p>
         </div>
         <Link as={`/post/${article.slug}`} href="/post/[id]">
           <a className="flex flex-row max-w-xl text-white text-xs items-center rounded-md bg-[#62A9FF] px-3 py-2 transition ease-in-out duration-200 transform hover:-translate-y-0.5">
