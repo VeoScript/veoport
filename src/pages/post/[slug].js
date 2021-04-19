e1d37beeff6d30a053cd8b89614225d38e3b1e80
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-19 18:34:50 +0800

    feat: change strapi to graphcms (#88)
    * refactor: remove cloudinary
    
    * feat: install graphql
    
    * refactor: delete components
    
    * refactor: delete strapi api and media holder
    
    * refactor: update blog page
    
    * Update yarn.lock

--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -1,80 +1,103 @@
-import Moment from 'react-moment'
-import { fetchAPI } from '~/lib/strapi/api'
-import Layout from '~/layouts/default'
-import Profiles from '~/components/profiles'
-import Image from 'next/image'
-import Seo from '~/components/seo'
-import { getStrapiMedia } from '~/lib/strapi/media' 
+import { GraphQLClient } from "graphql-request";
+import Link from "next/link";
 
-export default function Post({ article }) {
-  const imageUrl = getStrapiMedia(article.image)
-  
-  const seo = {
-    metaTitle: article.title,
-    metaDescription: article.description,
-    shareImage: article.image,
-    article: true
-  }
+const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
-  return (
-    <Layout>
-      <Seo seo={seo} />
-        <div className="flex flex-col items-center justify-start h-screen w-full overflow-y-auto px-3 pt-5 pb-20 space-y-5">
-          <div className="flex flex-row max-w-2xl mt-3 px-2.5">
-            <div className="w-3/5">
-              <Image 
-                className="rounded-md"
-                src={imageUrl}
-                alt={article.name}
-                width={500}
-                height={300}
-                layout="intrinsic"
-                loading="lazy"
-              />
-            </div>
-            <div className="flex flex-col items-end justify-end w-2/5 space-y-0.5">
-              <div className="flex flex-col md:flex-row items-center pb-5 space-x-2 space-y-2 md:space-y-0">
-                <Profiles author={article.author.picture} />
-                <div>
-                  <p className="font-bold text-base">{article.author.name}</p>
-                  <div className="font-light text-xs">
-                    <Moment format="MMMM DD, YYYY - hh:mm A">
-                      {article.created_at}
-                    </Moment>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col max-w-2xl mt-3 px-2.5 space-y-2">
-            <h1 className="font-bold text-xl">{article.title}</h1>
-            <p className="text-justify">{article.content}</p>
-          </div>
-        </div>
-    </Layout>
-  )
+export async function getStaticProps({ params }) {
+  const { post } = await graphcms.request(
+    `
+    query Post($slug: String!) {
+      post(where: { slug: $slug }) {
+        id
+        title
+        content{
+          text
+        }
+        slug
+        coverImage {
+          id
+          url
+        }
+        author {
+          id
+          name
+        }
+        date
+      }
+    }
+  `,
+    {
+      slug: params.slug,
+    }
+  );
+
+  return {
+    props: {
+      post,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const articles = await fetchAPI('/articles')
-
-  return {
-    paths: articles.map((article) => ({
-      params: {
-        slug: article.slug
+  const { posts } = await graphcms.request(`
+    {
+      posts {
+        id
+        title
+        content{
+          text
+        }
+        slug
+        coverImage {
+          id
+          url
+        }
+        author {
+          id
+          name
+        }
+        date
       }
-    })),
-    fallback: false
-  }
-}
-
-export async function getStaticProps({ params }) {
-  const articles = await fetchAPI(`/articles?slug=${params.slug}&status=published`)
+    }
+  `);
 
   return {
-    props: { 
-      article: articles[0],
-     },
-    revalidate: 1,
-  }
+    paths: posts.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
 }
+
+export default function Post ({ post }) {
+  return (
+    <div className="py-16 bg-gray-100 min-h-screen">
+      <div className="max-w-lg shadow-lg rounded-lg mx-auto mb-16">
+        <div
+          className="h-48 rounded-t flex-none bg-cover text-center overflow-hidden"
+          style={{ backgroundImage: `url(${post.coverImage.url})` }}
+          title={post.title}
+        />
+        <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-xl mb-2">
+              {post.title}
+            </div>
+            <p className="text-gray-700 text-base">{post.content.text}</p>
+          </div>
+          <div className="flex items-center">
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none">{post.author.name}</p>
+              <p className="text-gray-600">{post.date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-lg mx-auto">
+        <Link href="/blog">
+          <a>Back to all posts</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
