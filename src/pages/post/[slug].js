9ecb09fd14714245b8c794139e7b67dbbbd024c2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-20 21:07:07 +0800

    fix: content markdown

--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Link from "next/link"
 import { GraphQLClient } from "graphql-request"
 import Layout from '~/layouts/default'
+import ReactTooltip from 'react-tooltip'
 
 const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
@@ -14,7 +15,7 @@ export async function getStaticProps({ params }) {
         id
         title
         content{
-          text
+          html
         }
         slug
         coverImage {
@@ -48,7 +49,7 @@ export async function getStaticPaths() {
         id
         title
         content{
-          text
+          html
         }
         slug
         coverImage {
@@ -90,11 +91,12 @@ export default function Post ({ post }) {
                 />
                 <div className="bg-gray-100 dark:bg-gray-900 rounded-b-lg p-4 flex flex-col items-end leading-normal">
                   <div className="mb-8 -space-y-2">
-                    <div className="flex flex-row justify-between">
-                      <div className="flex flex-row items-center space-x-3">
-                        <div>
+                    <div className="flex flex-col md:flex-row justify-start">
+                      <div className="flex flex-row items-center space-x-2">
+                        <div className="hidden md:block">
+                          <ReactTooltip effect="solid" />
                           <Link href="/blog">
-                            <a data-tooltip="Back">
+                            <a data-tip="Back to Blog">
                               <svg class="w-6 h-6 transition ease-in-out duration-300 transform hover:-translate-x-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                             </a>
                           </Link>
@@ -103,22 +105,20 @@ export default function Post ({ post }) {
                           {post.title}
                         </div>
                       </div>
-                      <div className="text-sm space-y-1">
-                        <p className="font-semibold text-sm leading-none">{post.author.name}</p>
-                        <p className="font-light text-right text-xs text-gray-500">
-                          <Moment format="MMMM DD, YYYY">
-                            {post.date}
-                          </Moment>
-                        </p>
-                      </div>
                     </div>
-                    <p className="text-base text-justify pt-8">{post.content.text}</p>
-                  </div>
-                  <div className="flex flex-row items-center w-full">
-                    <div className="block md:hidden max-w-lg mx-auto">
-                      <Link href="/blog">
-                        <a className="flex flex-row max-w-xl items-center text-white text-xs rounded-md bg-[#62A9FF] px-3 py-2 transition ease-in-out duration-200 transform hover:-translate-y-0.5">Back to all posts...</a>
-                      </Link>
+                    <div className="flex-flex-col items-center text-base text-justify space-y-1 w-full h-full normal-case px-3 pt-5 md:pt-8" dangerouslySetInnerHTML={{ __html: post.content.html}} />
+                    <div className="flex flex-col items-start justify-center pl-3 pt-10 text-sm space-y-1">
+                      <p className="font-semibold text-base leading-none">{post.author.name}</p>
+                      <p className="font-light text-sm text-gray-500 ml-0.5">
+                        <Moment format="MMMM DD, YYYY">
+                          {post.date}
+                        </Moment>
+                      </p>
+                      <div className="block md:hidden -mt-3 max-w-lg ml-0.5">
+                        <Link href="/blog">
+                          <a className="text-xs transition ease-in-out duration-200 transform hover:underline">Back to blog</a>
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
