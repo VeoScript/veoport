618c1adae4c525f954b9010a078e2f27d3650d59
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-23 23:40:46 +0800

    style: add comment view responsive layout

--- src/components/commentview.js
+++ src/components/commentview.js
@@ -37,17 +37,17 @@ export default function Comments ({ postID }) {
           <h5 className="text-xs font-light text-gray-500"><span>50</span>&nbsp;Comments</h5>
         </div>
         <div className="flex flex-col w-full">
-          <div className="hidden md:flex flex-row w-full">
-            <div className="flex flex-col w-8/12 space-y-1">
-              <input type="text" name="commentor" {...register("commentor", { required: true })} className="px-5 py-3 bg-gray-200 dark:bg-gray-800 rounded-l-xl focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Your Name" disabled={ isSubmitting } />
-              { errors.commentor && <span className="font-medium text-xs tracking-wide text-red-600 mx-3">Name is required</span> }
+          <div className="flex flex-col md:flex-row w-full space-y-1 md:space-y-0">
+            <div className="flex flex-col w-full md:w-8/12 space-y-1">
+              <input type="text" name="commentor" {...register("commentor", { required: true })} className="px-5 py-3 text-xs md:text-sm bg-gray-200 dark:bg-gray-800 rounded-none md:rounded-l-xl focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Your Name" disabled={ isSubmitting } />
+              { errors.commentor && <span className="font-light md:font-medium text-xs tracking-wide text-red-600 mx-3">Name is required</span> }
             </div>
-            <div className="flex flex-col w-11/12 space-y-1">
-              <textarea type="text" name="comment" {...register("comment", { required: true })} className="px-5 py-3 bg-gray-200 dark:bg-gray-800 rounded-none border-l border-gray-400 focus:outline-none resize-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Comment" rows="1" disabled={ isSubmitting } />
-              { errors.comment && <span className="font-medium text-xs tracking-wide text-red-600 mx-3">Comment is required</span> }
+            <div className="flex flex-col w-full md:w-11/12 space-y-1">
+              <textarea type="text" name="comment" {...register("comment", { required: true })} className="px-5 py-3 text-xs md:text-sm bg-gray-200 dark:bg-gray-800 rounded-none border-l border-transparent md:border-gray-400 focus:outline-none resize-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Comment" rows="1" disabled={ isSubmitting } />
+              { errors.comment && <span className="font-light md:font-medium text-xs tracking-wide text-red-600 mx-3">Comment is required</span> }
             </div>
-            <div className="flex flex-col w-3/12">
-              <button type="submit" className="px-10 py-3 bg-gray-200 dark:bg-gray-800 rounded-r-xl border-l border-gray-400 focus:outline-none" disabled={ isSubmitting }>
+            <div className="flex flex-col items-end md:items-start w-full md:w-3/12">
+              <button type="submit" className="px-10 py-3 text-xs md:text-sm bg-gray-200 dark:bg-gray-800 rounded-none md:rounded-r-xl border-l border-transparent md:border-gray-400 focus:outline-none" disabled={ isSubmitting }>
                 { isSubmitting ? <LoadingButton /> : 'Go' }
               </button>
             </div>
@@ -61,7 +61,7 @@ export default function Comments ({ postID }) {
 function LoadingButton () {
   return (
     <div className="flex items-center text-xs space-x-3">
-      <svg className="w-5 h-5 fill-current" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="#000000">
+      <svg className="w-5 h-5 fill-current text-[#333] dark:text-white" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="#000000">
         <g transform="translate(1 1)" fillRule="evenodd">
           <circle cx="5" cy="50" r="5">
             <animate attributeName="cy" begin="0s" dur="2.2s" values="50;5;50;50" calcMode="linear" repeatCount="indefinite"></animate>
