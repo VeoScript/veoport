923e2315fe391c64391c53fe0839850c74b2253a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-23 15:28:28 +0800

    refactor: import navbar

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,4 +1,4 @@
-import Navbar from '~/components/navbar'
+import Navbar from '~/components/Navbar'
 import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 export default function Layout ({ children }) {
