24e8a594bd8c9f662e8ac3636ce1f5a60e91e69c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-23 22:55:35 +0800

    chore: update post slug page

--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -5,6 +5,7 @@ import { GraphQLClient } from "graphql-request"
 import Layout from '~/layouts/default'
 import ReactTooltip from 'react-tooltip'
 import CommentsSection from '~/components/commentview'
+import CommentsDisplay from '~/components/commentdisplay'
 
 const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
@@ -89,6 +90,8 @@ export async function getStaticPaths() {
 
 export default function Post ({ post }) {
   const getComments = post.comments
+  // const getOneComment = getComments[getComments.length - 1]
+  // console.log(getOneComment.id)
   return (
     <>
       <Head>
@@ -139,22 +142,11 @@ export default function Post ({ post }) {
                         <CommentsSection postID={post.id} />
                         <div className="flex flex-col justify-center w-full pl-3 pt-5">
                           <div className="flex flex-col justify-center space-y-3">
-                            {getComments.map((comm, i) => {
+                            {getComments.map((comm) => {
+                              console.log(comm.id)
                               return (
-                                <div>
-                                  <div className="flex flex-row items-center space-x-3" key={i}>
-                                    <div>
-                                      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
-                                      </svg>
-                                    </div>
-                                    <div className="flex flex-col space-y-1">
-                                      <div className="font-bold text-sm">{comm.commentor}</div>
-                                      <div className="font-semibold text-gray-500 text-xs">{comm.comment}</div>
-                                    </div>
-                                  </div>
-                                  <div className="flex flex-row mt-3 border-t border-gray-200 dark:border-gray-800"></div>
-                                </div>
+                                // Comment Display
+                                <CommentsDisplay commentID={comm.id} comment={comm.comment} commentor={comm.commentor} />
                               )
                             })}
                           </div>
