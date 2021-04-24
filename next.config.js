045d6c260a8ffe9a9d3e01dea25bccba5eaa1dbe
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-24 11:16:04 +0800

    refactor: add environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   future: {
     webpack5: true,
+  },
+  env: {
+    GRAPHQL_URL_ENDPOINT: process.env.GRAPHQL_URL_ENDPOINT,
+    VEOPORTAL_MUTATIONS: process.env.VEOPORTAL_MUTATIONS,
+    GMAIL_SERVICE_ID: process.env.GMAIL_SERVICE_ID,
+    GMAIL_USER_ID: process.env.GMAIL_USER_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID
   }
 }
