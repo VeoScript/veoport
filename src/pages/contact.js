e61ceb28f3e5dc29c89500de0e6053992f792ad1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-24 11:15:41 +0800

    refactor: change promise to async function
    Closes #115

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,22 +9,24 @@ import { info } from '~/static/contact'
 
 export default function Contact() {
   const { register, handleSubmit, reset, formState: { errors, isSubmitting }} = useForm()
-  const onSubmit = (formData) => {
-    return new Promise((resolve) => {
-      emailjs.send("service_qgx6gh5","template_i014bb9", formData, 'user_7nMpeK2S1XP8OhBYWh6gO')
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+  const sendEmail = async ({ name, email, message }) => {
+    try {
+      const mail = await emailjs.send(
+        process.env.GMAIL_SERVICE_ID,
+        process.env.TEMPLATE_ID,
+        { name, email, message },
+        process.env.GMAIL_USER_ID
+      )
+      if (mail) {
         toast.info('You email was successfully sent! Thank you for reaching out.')
         reset()
-      }, (err) => {
-        console.log('FAILED...', err);
-        toast.error('Failed to send email! Try again.')
+      } else {
+        toast.error('Something went wrong try again.')
         reset()
-      })
-      setTimeout(() => {
-        resolve
-      }, 3000)
-    })
+      }
+    } catch (err) {
+      console.error(err)
+    }
   }
   return (
     <>
@@ -34,11 +36,19 @@ export default function Contact() {
       <Layout>
         <div className="flex justify-center items-center w-full h-screen overflow-y-auto py-10 space-x-0 space-y-5 md:space-x-5 md:space-y-0">
           <ToastContainer draggablePercent={60} />
-          <div className="flex flex-col md:flex-row justify-around items-center max-width-xl px-10 py-10">
+          <div className="flex flex-col md:flex-row justify-around items-center w-full md:w-11/12 px-5 py-10 md:px-10 md:py-10 space-y-3 md:space-y-0">
             <div className="w-full mx-auto md:w-2/5 md:space-y-3 space-y-1">
-              <h1 className="md:text-4xl text-lg text-[#333] font-bold dark:text-white">Get in touch</h1>
-              <p className="md:text-xl text-xs text-[#333] dark:text-white">If you got a project in mind, why not get in touch. Let's work together, I'm available for interesting freelance work projects.</p>
-              <div className="flex flex-col space-y-1 md:space-y-3 py-5">
+              <div className="md:hidden flex flex-col py-5">
+                {info.map(({ icon, content }, i) => (
+                  <div className="flex flex-row items-center space-x-3" key={i}>
+                    <div>{ icon }</div>
+                    <div className="font-light text-xs md:font-normal md:text-base">{ content }</div>
+                  </div>
+                ))}
+              </div>
+              <h1 className="md:text-4xl text-2xl text-[#333] font-bold dark:text-white">Get in touch</h1>
+              <p className="md:text-xl text-base text-[#333] dark:text-white">If you got a project in mind, why not get in touch. Let's work together, I'm available for interesting freelance work projects.</p>
+              <div className="hidden md:flex flex-col space-y-1 md:space-y-3 py-5">
                 {info.map(({ icon, content }, i) => (
                   <div className="flex flex-row items-center space-x-3" key={i}>
                     <div>{ icon }</div>
@@ -47,22 +57,22 @@ export default function Contact() {
                 ))}
               </div>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className="w-full md:w-2/5 mx-auto space-y-3">
+            <form onSubmit={handleSubmit(sendEmail)} className="w-full md:w-2/5 mx-auto space-y-3">
               <div className="form-control">
-                <input type="text" name="name" {...register("name", { required: true })} className="bg-gray-100 text-[#333] text-xs md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Name" disabled={ isSubmitting } />
+                <input type="text" name="name" {...register("name", { required: true })} className="bg-gray-100 text-[#333] text-base md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Name" disabled={ isSubmitting } />
                 { errors.name && <span className="font-medium text-xs tracking-wide text-red-500 mx-3">Name is required!</span> }
               </div>
               <div className="form-control">
-                <input type="email" name="email" {...register("email", { required: true })} className="bg-gray-100 text-[#333] text-xs md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Email" disabled={ isSubmitting } />
+                <input type="email" name="email" {...register("email", { required: true })} className="bg-gray-100 text-[#333] text-base md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Email" disabled={ isSubmitting } />
                 { errors.email && <span className="font-medium text-xs tracking-wide text-red-500 mx-3">Email is required!</span> }
               </div>
               <div className="form-control">
-                <textarea name="message" {...register("message", { required: true })} className="bg-gray-100 text-[#333] text-xs md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Message here..." rows="3" disabled={ isSubmitting }></textarea>
+                <textarea name="message" {...register("message", { required: true })} className="bg-gray-100 text-[#333] text-base md:text-lg px-3 py-2 md:px-5 md:py-3 w-full rounded-lg focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" placeholder="Message here..." rows="3" disabled={ isSubmitting }></textarea>
                 { errors.message && <span className="font-medium text-xs tracking-wide text-red-500 mx-3">Message is required!</span> }
               </div>
               <div className="form-control flex justify-end">
                 <motion.button
-                  className="bg-[#62A9FF] text-white text-xs md:text-sm px-5 py-2 md:px-5 md:py-2 -mt-2 rounded-lg transition ease-in-out duration-200 transform hover:-translate-y-0.5 focus:outline-none"
+                  className="bg-[#62A9FF] text-white text-base md:text-sm px-5 py-2 md:px-5 md:py-2 -mt-2 rounded-lg transition ease-in-out duration-200 transform hover:-translate-y-0.5 focus:outline-none"
                   whileHover={{ y: -4 }}
                   disabled={ isSubmitting }
                 >
