b4fe38d2e99d6dbf261fe055cfc965b417a13a2d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-11 16:26:43 +0800

    style: adjust blog post cards max-width (#157)

--- src/pages/[title].jsx
+++ src/pages/[title].jsx
@@ -51,7 +51,7 @@ export default function BlogContent({ title, all_users, online_user, get_blog_po
                 <div className="flex flex-col w-full p-6 space-y-5">
                   <div className="flex flex-row items-center justify-between w-full space-x-5">
                     <div className="flex flex-col-reverse md:flex-col w-full max-w-full space-y-0 md:space-y-3">
-                      <div className="flex justify-center md:justify-start w-full mt-5 md:mt-0">
+                      <div className="flex justify-center md:justify-start w-full max-w-full md:max-w-2xl mt-5 md:mt-0">
                         <span className="font-bold text-3xl">{ title }</span>
                       </div>
                       <div className="flex flex-col md:flex-row items-center justify-between w-full">
