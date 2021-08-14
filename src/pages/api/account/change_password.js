250848acce45ce13ada6d48177a8a0f1a6a21421
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-14 11:50:41 +0800

    feat: add password encryption

--- src/pages/api/account/change_password.js
+++ src/pages/api/account/change_password.js
@@ -1,13 +1,18 @@
+import bcrypt from 'bcryptjs'
 import prisma from '~/lib/prisma'
 
 export default async function handler(req, res) {
   if (req.method === 'PUT') {
+    const raw_password = req.body.new_password
+    const salt = await bcrypt.genSalt()
+    const password = await bcrypt.hash(raw_password, salt)
+
     const change_password = await prisma.user.update({
       where: {
         id: req.body.id
       },
       data: {
-        password: req.body.new_password
+        password: password
       }
     })
     res.json(change_password)
