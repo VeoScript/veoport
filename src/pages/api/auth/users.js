9f9974ece5250a6c8fb018af61a83a554683584e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-18 10:19:28 +0800

    refactor: add id in users prisma-query select

--- src/pages/api/auth/users.js
+++ src/pages/api/auth/users.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const all_users = await prisma.user.findMany({
       select: {
+        id: true,
         email: true,
         username: true,
         password: true
