6588037ee4971ffd7d8e737439d02f3ba62c01ee
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-18 10:12:52 +0800

    refactor: users prisma-query selects only username and password (#173)

--- src/pages/api/auth/users.js
+++ src/pages/api/auth/users.js
@@ -2,7 +2,12 @@ import prisma from '~/lib/prisma'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const all_users = await prisma.user.findMany()
+    const all_users = await prisma.user.findMany({
+      select: {
+        username: true,
+        password: true
+      }
+    })
     res.json(all_users)
   } else {
     res.send('get method only...')
