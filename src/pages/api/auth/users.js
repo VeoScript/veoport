cae5bc94ef9a24f12f20179fd42aebe4beb64cb5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-18 10:17:17 +0800

    refactor: add email in users prisma-query select

--- src/pages/api/auth/users.js
+++ src/pages/api/auth/users.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const all_users = await prisma.user.findMany({
       select: {
+        email: true,
         username: true,
         password: true
       }
