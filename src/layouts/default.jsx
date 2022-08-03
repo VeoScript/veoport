a6066bad811205dec74cbe90743f8bb741113b5b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-08-03 11:34:35 +0800

    style: max width move to children container (#200)

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -3,9 +3,9 @@ import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 export default function Layout ({ children }) {
   return (
-    <div className="font-poppins flex flex-col justify-center items-center w-full max-w-[1401px] h-screen transition ease-in-out duration-700 overflow-y-auto bg-white text-[#333] dark:bg-[#232733] dark:text-white">
+    <div className="font-poppins flex flex-col justify-center items-center w-full max-w-full h-screen transition ease-in-out duration-700 overflow-y-auto bg-white text-[#333] dark:bg-[#232733] dark:text-white">
       <Navbar />
-      <div className="flex flex-row justify-center w-full max-w-full h-auto">
+      <div className="flex flex-row justify-center w-full max-w-[1401px] h-auto">
         { children }
         <MessengerCustomerChat
           pageId="101914628707986"
