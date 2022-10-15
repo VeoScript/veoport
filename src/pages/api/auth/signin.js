3108c7caac4db8e947e5ba965df83a32b52a3759
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-10-15 12:30:09 +0800

    refactor: move signin and signup logic to server side (#216)
    To secure authentication signin and signup
    
    Closes #201

--- src/pages/api/auth/signin.js
+++ src/pages/api/auth/signin.js
@@ -1,19 +1,38 @@
 import { withIronSession } from 'next-iron-session'
+import bcrypt from 'bcryptjs'
 import prisma from '~/lib/prisma'
 
 async function handler(req, res) {
+  const { username, password } = req.body
+
   const findUser = await prisma.user.findMany({
     where: {
-      username: req.body.username
+      username: username
     },
     select: {
       id: true,
-      username: true
+      username: true,
+      password: true
     }
   })
 
+  if (!findUser[0]) {
+    return res.status(400).json({
+      message: 'This account is not registered!'
+    })
+  }
+
   const getId = findUser[0].id
   const getUsername = findUser[0].username
+  const getHashedPassword = findUser[0].password
+
+  const matchedPassword = await bcrypt.compare(password, getHashedPassword)
+
+  if (!matchedPassword) {
+    return res.status(400).json({
+      message: 'Incorrect password!'
+    })
+  }
 
   req.session.set('user', {
     id: getId,
