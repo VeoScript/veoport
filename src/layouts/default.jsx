2124b92977e806ad001e1b0370474e87e85d2c75
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-10-15 22:29:11 +0800

    style: adjust max width in main layout

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -5,7 +5,7 @@ export default function Layout ({ children }) {
   return (
     <div className="font-poppins flex flex-col justify-center items-center w-full max-w-full h-screen transition ease-in-out duration-700 overflow-y-auto bg-white text-[#333] dark:bg-[#232733] dark:text-white">
       <Navbar />
-      <div className="flex flex-row justify-center w-full max-w-[1401px] h-auto">
+      <div className="flex flex-row justify-center w-full max-w-[1450px] h-auto">
         { children }
         <MessengerCustomerChat
           pageId={process.env.MESSENGER_PAGE_ID}
